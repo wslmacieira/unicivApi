d0685e66b039dd9a5e45f2ec5ae7ea24cbe3bb07
wslmacieiradev <wslmacieira@outlook.com>
2020-12-31 12:07:44 -0300

    cria FundoCapital model

---
+++ Uniciv.Api/Models/FundoCapital.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Uniciv.Api.Models
+{
+    public class FundoCapital
+    {
+        public FundoCapital()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public decimal ValorNecessario { get; set; }
+        public decimal ValorAtual { get; set; }
+        public DateTime? DataResgate { get; set; }
+    }
+}

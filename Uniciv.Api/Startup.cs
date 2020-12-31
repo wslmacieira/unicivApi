6a732a90f70424a1842dd6232ca22e5678fc29e8
wslmacieiradev <wslmacieira@outlook.com>
2020-12-31 11:48:06 -0300

    Adiciona Mvc e Cors

--- Uniciv.Api/Startup.cs
+++ Uniciv.Api/Startup.cs
@@ -15,6 +15,7 @@ namespace Uniciv.Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +26,14 @@ namespace Uniciv.Api
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseCors(config =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                config.AllowAnyHeader();
+                config.AllowAnyMethod();
+                config.AllowAnyOrigin();
             });
+
+            app.UseMvc();
         }
     }
 }
